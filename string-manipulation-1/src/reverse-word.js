ac09ee40ab9e4328bcd48376ecd2c7d901cac87f
James Yang <jamesdaeyang@gmail.com>
2022-10-21 19:22:43 +0000

    Updated with pseudocode.

--- string-manipulation-1/src/reverse-word.js
+++ string-manipulation-1/src/reverse-word.js
@@ -1,8 +1,16 @@
 /* exported reverseWord */
 
+// define function reverseWord
+// assign empty string to new variable
+// add the value of the string at last index to new variable
+
 function reverseWord(word) {
   var reverse = '';
   for (var i = word.length - 1; i >= 0; i--) {
     reverse += word[i];
   } return reverse;
 }
+
+// for loop to iterate through given string
+// for loop begins at the last index of string and continues until index 0
+// during each loop the value of string at index i is added to reverse string
