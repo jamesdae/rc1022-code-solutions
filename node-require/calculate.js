06f8bfeca968bba2a11aa5d801cffbb72561eab1
James Yang <jamesdaeyang@gmail.com>
2022-11-30 00:01:13 +0000

    Completed node-require exercise

---
+++ node-require/calculate.js
@@ -0,0 +1,21 @@
+const add = require('./add');
+const subtract = require('./subtract');
+const divide = require('./divide');
+const multiply = require('./multiply');
+
+switch (process.argv[3]) {
+  case 'plus':
+    console.log('result:', add(Number(process.argv[2]), Number(process.argv[4])));
+    break;
+  case 'minus':
+    console.log('result:', subtract(Number(process.argv[2]), Number(process.argv[4])));
+    break;
+  case 'over':
+    console.log('result:', divide(Number(process.argv[2]), Number(process.argv[4])));
+    break;
+  case 'times':
+    console.log('result:', multiply(Number(process.argv[2]), Number(process.argv[4])));
+    break;
+  default:
+    break;
+}
