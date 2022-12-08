19b8c92c7c6e94a93d8975ca5cb1b801e69d17f9
James Yang <jamesdaeyang@gmail.com>
2022-12-08 21:59:35 +0000

    Made requested changes

--- sql-join/where-are-they.sql
+++ sql-join/where-are-they.sql
@@ -2,17 +2,6 @@ select "line1",
        "cities"."name" as "city",
        "district",
        "countries"."name" as "country"
-  from "customers"
-  join "addresses" using ("addressId")
+  from "addresses"
   join "cities" using ("cityId")
-  join "countries" using ("countryId")
- where "customerId" = 237
-   or  "customerId" = 592
-   or  "customerId" = 116
-   or  "customerId" = 195
-   or  "customerId" = 305
-   or  "customerId" = 591
-   or  "customerId" = 362
-   or  "customerId" = 204
-   or  "customerId" = 196
-   or  "customerId" = 13;
+  join "countries" using ("countryId");
