230bc2eb1c35baba8f0daabb0d81d6d2572faaf7
James Yang <jamesdaeyang@gmail.com>
2022-12-12 17:32:44 +0000

    Completed array-filter exercise

---
+++ array-filter/main.js
@@ -0,0 +1,25 @@
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const names = [
+  'Ada',
+  'Hedy',
+  'Jean',
+  'Grace',
+  'Evelyn',
+  'Joan',
+  'Elizabeth',
+  'Janese',
+  'Donna'
+];
+
+const evenNumbers = numbers.filter(eachNumber => eachNumber % 2 === 0);
+console.log(evenNumbers);
+
+const overFive = numbers.filter(eachNumber => eachNumber > 5);
+console.log(overFive);
+
+const startWithE = names.filter(eachName => eachName.startsWith('E'));
+console.log(startWithE);
+
+const haveD = names.filter(eachName => eachName.toLowerCase().includes('d'));
+console.log(haveD);
